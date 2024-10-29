9f17c7ce058c719400f8f8f3af3d257d798c44af
The-HonouredOne <surendrajunwal852@gmail.com>
2024-10-29 07:33:36 +0530

    message

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import './App.css';
 
 function App() {
   return (
-   <>ccbxvcbvvm,v</>
+   <>
+   <h1>The USEEFECT hook allow you to perform side effects in your components</h1>
+   <p>some examples of side effects are:-</p>
+   <ul>
+    <li>Fetching data from API</li>
+    <li>Directly updating the DOM</li>
+    <li>Timers like Settimeout and SetInterval</li>
+   </ul>
+   <h3>To use USEEFFECT we need two things </h3>
+    <ul><li>first is callback function. (this is necessary)</li>
+    <li>second is Dependencies(This is optional)</li></ul>
+   
+   
+   
+   
+   </>
   );
 }
 
